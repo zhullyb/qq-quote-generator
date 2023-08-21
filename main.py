813b7f485f8f2fa9a872275a26ccc20d01287554
zhullyb <zhullyb@outlook.com>
2023-08-21 23:15:57 +0200

    use json now

--- main.py
+++ main.py
@@ -4,23 +4,18 @@ from utils import *
 from screenshot import Screenshot
 app = Flask(__name__)
 
+data_list = []
+
 @app.after_request
 def set_headers(response):
     response.headers["Referrer-Policy"] = 'no-referrer'
     return response
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    # 接收 user_id 参数, nickname 参数
-    user_id = request.args.get('user_id')
-    nickname = request.args.get('nickname')
-    # 调用 screenshot 函数
-    if request.args.get('message'):
-        message = request.args.get('message')
-        ss_get = ss.screenshot(message=message, user_id=user_id, nickname=nickname)
-    elif request.args.get('image_url'):
-        image_url = request.args.get('image_url')
-        ss_get = ss.screenshot(image_url=image_url, user_id=user_id, nickname=nickname)
+def handler():
+    global data_list
+    data_list = request.get_json()
+    ss_get = ss.screenshot()
     if Config.RETURN_PNG:
         return send_file(io.BytesIO(ss_get), mimetype='image/png')
     else:
@@ -28,18 +23,11 @@ def index():
 
 @app.route('/quote/', methods=['GET', 'POST'])
 def quote():
-    # 接收 user_id 参数, nickname 参数
-    user_id = request.args.get('user_id')
-    nickname = request.args.get('nickname')
-    avatar_url = 'https://q1.qlogo.cn/g?b=qq&nk={}&s=640'.format(user_id)
-    if request.args.get('message'):
-        message = request.args.get('message')
-        return render_template('single-message.html', message=message, user_id=user_id, nickname=nickname, avatar_url=avatar_url)
-    # 如果存在 image_url 参数，则获取 image_url 参数
-    elif request.args.get('image_url'):
-        image_url = request.args.get('image_url')
-        return render_template('single-image.html', user_id=user_id, nickname=nickname, avatar_url=avatar_url, image_url=image_url)
-
+    global data_list
+    return render_template('main-template.html', data_list=data_list)
+    
 if __name__ == '__main__':
+    # init headless browser
     ss = Screenshot()
+    
     app.run(host='0.0.0.0', port=Config.FLASK_RUN_PORT, debug=True)
--- screenshot.py
+++ screenshot.py
@@ -10,13 +10,9 @@ class Screenshot:
         opts.add_argument("--headless")
         self.driver = webdriver.Firefox(options=opts)
 
-    def screenshot(self, user_id, nickname, **kwargs):
-        if kwargs.get('image_url'):
-            image_url = kwargs.get('image_url')
-            self.driver.get("http://127.0.0.1:{}/quote/?user_id={}&nickname={}&image_url={}".format(Config.FLASK_RUN_PORT, user_id, nickname, image_url))
-        elif kwargs.get('message'):
-            message = kwargs.get('message')
-            self.driver.get("http://127.0.0.1:{}/quote/?user_id={}&nickname={}&message={}".format(Config.FLASK_RUN_PORT, user_id, nickname, message))
+    def screenshot(self):
+        self.driver.get("http://127.0.0.1:{}/quote/".format(Config.FLASK_RUN_PORT))
+        
         if Config.RETURN_PNG:
             return self.driver.find_element(By.ID, "app").screenshot_as_png
         else:
