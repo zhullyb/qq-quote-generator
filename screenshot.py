c41fcabca5559421b01e30f1d62a05d5f3a4dedf
zhullyb <zhullyb@outlook.com>
2023-11-16 08:49:16 +0800

    add __del__ function to avoid memory leak

--- screenshot.py
+++ screenshot.py
@@ -8,12 +8,15 @@ class Screenshot:
     def __init__(self):
         # Set the headless option.
         opts = Options()
-        opts.add_argument("--headless")
+        # opts.add_argument("--headless")
         if Config.GECKODRIVER_PATH:
             service = Service(executable_path=Config.GECKODRIVER_PATH)
             self.driver = webdriver.Firefox(options=opts, service=service)
         else:
             self.driver = webdriver.Firefox(options=opts)
+            
+    def __del__(self):
+        self.driver.quit()
 
     def screenshot(self, ret_type, unique_id):
         self.driver.get(f"http://127.0.0.1:{Config.FLASK_RUN_PORT}/quote/?id={unique_id}")
